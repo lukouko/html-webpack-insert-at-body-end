584f28952f2ca1bcc94c3840588f30a1da796358
lmarsden <luke.marsden@smokeball.com>
2017-05-11 14:56:35 +1000

    Fixed issue where callback is not called

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ InsertAtBodyEnd.prototype.apply = function(compiler) {
   const that = this;
   compiler.plugin('compilation', function(compilation) { 
     compilation.plugin('html-webpack-plugin-after-html-processing', function(htmlPluginData, callback) {
-      if (htmlPluginData.plugin.options.filename !== that.options.filename) return;
+      if (htmlPluginData.plugin.options.filename !== that.options.filename) return callback();
 
       const scriptTag = '  <script src="' + that.options.scriptSrc + '" type="text/javascript"></script>\n'
       const modifiedHtml = htmlPluginData.html.replace(CLOSE_BODY_TAG, scriptTag + CLOSE_BODY_TAG);
